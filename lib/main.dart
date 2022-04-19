d911363dec859b215a95e39243f16d581afb2696
ebrar <92300256+ebrarakcaseven@users.noreply.github.com>
2022-04-19 15:47:48 +0300

    login page eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_code_app/nav.dart';
+import 'package:qr_code_app/pages/login.dart';
 
 void main() async {
   try {
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Nav(),
+      home: LoginPage(),
     );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:qr_code_app/nav.dart';
 import 'package:qr_code_app/service/service.dart';
 
 class Home extends StatefulWidget {
